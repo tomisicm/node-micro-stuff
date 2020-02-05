1c8d8a4b25bafc300db8e171b019c60f215f9108
tomisicm <m.tomisic@beeit.rs>
2020-02-05 16:42:06 +0100

    completing listing service

--- listings-service/src/server/startServer.js
+++ listings-service/src/server/startServer.js
@@ -1,8 +1,9 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-
 import accessEnv from "#root/helpers/accessEnv";
+
+import setupRoutes from "./routes";
 
 const PORT = accessEnv("PORT", 7100);
 
@@ -16,6 +17,8 @@ app.use(
         credentials: true
     })
 );
+
+setupRoutes(app);
 
 app.listen(PORT, "0.0.0.0", () => {
     console.info(`Listing service listening on port ${PORT}`);
