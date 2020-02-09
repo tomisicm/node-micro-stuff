e8a1071ca560f930f0c506ae8c02068b4858facf
tomisicm <tomisicm@protonmail.com>
2020-02-10 00:24:16 +0100

    comit from root

--- users-service/src/db/connection.js
+++ users-service/src/db/connection.js
@@ -4,15 +4,12 @@ import accessEnv from "#root/helpers/accessEnv";
 
 const DB_URI = accessEnv("DB_URI");
 
-const sequelize = new Sequelize(
-    DB_URI,
-    {
-        dialectOptions: {
-            charset: "utf8",
-            multipleStatements: true
-        },
-        logging: false
-    }
-);
+const sequelize = new Sequelize(DB_URI, {
+  dialectOptions: {
+    charset: "utf8",
+    multipleStatements: true
+  },
+  logging: false
+});
 
 export default sequelize;
--- users-service/src/server/startServer.js
+++ users-service/src/server/startServer.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import express from "express";
 
 import accessEnv from "#root/helpers/accessEnv";
-import setupRoutes from './routes';
+import setupRoutes from "./routes";
 
 const PORT = accessEnv("PORT", 7101);
 
@@ -12,20 +12,20 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(
-    cors({
-        origin: (origin, cb) => cb(null, true),
-        credentials: true
-    })
+  cors({
+    origin: (origin, cb) => cb(null, true),
+    credentials: true
+  })
 );
 
 setupRoutes(app);
 
 app.use((err, req, res, next) => {
-    return res.status(500).json({
-        message: err.message
-    });
+  return res.status(500).json({
+    message: err.message
+  });
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.info(`Users service listening on port ${PORT}`);
-})
+  console.info(`Users service listening on port ${PORT}`);
+});
